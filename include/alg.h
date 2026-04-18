503b43a819308c1cce00dc4b81d4c72d70689e9b
forcixxx <cixfor868@gmail.com>
2026-04-18 23:30:26 +0300

    Ласточкин Олег ист3

--- include/alg.h
+++ include/alg.h
@@ -1,10 +1,42 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_ALG_H_
-#define INCLUDE_ALG_H_
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
 
-#include <string>
+template<typename T, int MaxSize>
+class TStack {
+ private:
+  T elements[MaxSize];
+  int topIndex;
 
-std::string infx2pstfx(std::string);
-int eval(std::string);
+ public:
+  TStack() : topIndex(-1) {}
 
-#endif  // INCLUDE_ALG_H_
+  void push(T value) {
+    if (topIndex < MaxSize - 1) {
+      elements[++topIndex] = value;
+    }
+  }
+
+  void pop() {
+    if (topIndex >= 0) {
+      --topIndex;
+    }
+  }
+
+  T get() const {
+    if (topIndex >= 0) {
+      return elements[topIndex];
+    }
+    return T();
+  }
+
+  bool isempty() const {
+    return topIndex == -1;
+  }
+
+  int stack_size() const {
+    return topIndex + 1;
+  }
+};
+
+#endif  // INCLUDE_TSTACK_H_
