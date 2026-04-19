d38defc6e2c7b836b1c38d453f7e10fcb8e1c3e0
forcixxx <cixfor868@gmail.com>
2026-04-19 22:57:35 +0300

    Ласточкин О.О 25-ист-3

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,41 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template<typename T, int kSize>
 class TStack {
-  // добавьте код стека
+ private:
+    T data[kSize];
+    int topIndex;
+
+ public:
+    TStack() : topIndex(-1) {}
+
+    void addToStack(const T& value) {
+        if (stackIsFull()) return;
+        data[++topIndex] = value;
+    }
+
+    T takeFromStack() {
+        if (stackIsEmpty()) return T();
+        return data[topIndex--];
+    }
+
+    T lookAtTop() const {
+        if (stackIsEmpty()) return T();
+        return data[topIndex];
+    }
+
+    bool stackIsEmpty() const {
+        return topIndex == -1;
+    }
+
+    bool stackIsFull() const {
+        return topIndex == kSize - 1;
+    }
+
+    void clearStack() {
+        topIndex = -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
